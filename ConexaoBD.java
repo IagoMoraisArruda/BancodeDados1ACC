1a3997344a8ee398a1415f525b661542f38a7b28
Elder Alencar <86540840+ElderOFC@users.noreply.github.com>
2026-07-08 09:46:22 -0300

    Clear database user and password fields

--- ConexaoBD.java
+++ ConexaoBD.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class ConexaoBD {
 
     private static final String URL = "jdbc:mysql://acc-unifesspa-bd.mysql.database.azure.com:3306/acc_database?useSSL=true&requireSSL=false&serverTimezone=UTC";
-    private static final String USUARIO = "admin_acc";
+    private static final String USUARIO = "";
     private static final String SENHA = "";
 
     public static Connection conectar() throws SQLException {
